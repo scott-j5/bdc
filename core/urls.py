7d66b737bec858e6ebda2c289f238dd9f69e97b8
Scott James <scotty.james95@icloud.com>
2021-06-26 00:49:09 +1000

    continued development of homepage design

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 
 from .views import (
-    home_view
+    home_view,
+	vans_view,
 )
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('home/', home_view, name='home'),
+	path('vans/', home_view, name='vans-list'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home_view(request):
     context = {}
     return render(request, 'core/home.html', context)
+
+def vans_view(request):
+    context = {}
+    return render(request, 'core/vans.html', context)
