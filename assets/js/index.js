045cf77d08897f5a3b82f0487c0a76f92fe800cb
Scott James <scotty.james95@icloud.com>
2021-07-07 01:40:30 +1000

    updated nav bar

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,11 @@
 import "./modules/bootstrap";
 import "./modules/feather";
 
+//Styles
 import "../scss/app.scss";
 
+//Theme
 import "./modules/soft-design-system-pro";
+
+// 3rd party libraries
+import "./modules/rellax";
