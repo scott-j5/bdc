fe2ad55baeac874b2b62e62ed074654f3501203c
Scott James <scotty.james95@icloud.com>
2021-10-28 19:19:44 +1100

    Refactored functionality of profile pages/ separated emails to separate page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
 
 from .views import (
-    ProfileDetail
+    ProfileDetail,
+	ProfileConnectionsView,
+	ProfileEmailView
 )
 
 urlpatterns = [
     path('profile/<int:pk>/', ProfileDetail.as_view(), name='profile'),
 	path('profile/', ProfileDetail.as_view(), name='profile'),
+	path('profile/connections/', ProfileConnectionsView.as_view(), name='profile-connections'),
+	path('profile/email/', ProfileEmailView.as_view(), name='profile-email'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,11 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse, reverse_lazy
 from django.views.generic import DetailView, ListView, UpdateView, DeleteView
+
+from allauth.account.views import EmailView
+from allauth.socialaccount.views import ConnectionsView
 
 from .models import UserProfile
 # Create your views here.
@@ -11,8 +15,18 @@ from .models import UserProfile
 class ProfileDetail(LoginRequiredMixin, DetailView):
 	model = UserProfile
 	template_name = 'accounts/profile_detail.html'
+	success_url = reverse_lazy("profile")
 
 	def get(self, *args, **kwargs):
 		if not self.kwargs.get('pk', False):
 			self.kwargs['pk'] = self.request.user.id
 		return super().get(*args, **kwargs)
+
+
+class ProfileConnectionsView(LoginRequiredMixin, ConnectionsView):
+	template_name = 'accounts/profile_connections.html'
+
+
+class ProfileEmailView(LoginRequiredMixin, EmailView):
+	template_name = 'accounts/profile_email.html'
+	success_url = reverse_lazy("profile-email")
