1388e13708304bc39bdef604b6612ffc9a5f0ec5
Scott James <scotty.james95@icloud.com>
2021-10-28 15:28:15 +1100

    Continued workings on profile template

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,14 @@
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView, ListView, UpdateView, DeleteView
-
-from django.contrib.auth.models import User
 
 from .models import UserProfile
 # Create your views here.
 
 
-class ProfileDetail(DetailView):
+class ProfileDetail(LoginRequiredMixin, DetailView):
 	model = UserProfile
 	template_name = 'accounts/profile_detail.html'
 
